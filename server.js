85df6d421e4e390bb35d479b6ba116377efaa526
imAcni <101078007+imAcni@users.noreply.github.com>
2025-02-17 17:32:23 -0500

    v1
    asd

---
+++ server.js
@@ -0,0 +1,31 @@
+const express = require("express");
+const { createServer } = require("http");
+const { Server } = require("socket.io");
+const cors = require("cors");
+
+const app = express();
+app.use(cors());
+
+const server = createServer(app);
+const io = new Server(server, {
+    cors: {
+        origin: "*",
+        methods: ["GET", "POST"]
+    }
+});
+
+io.on("connection", (socket) => {
+    console.log("A user connected:", socket.id);
+
+    socket.on("playerTyped", (data) => {
+        io.emit("updateGame", data);
+    });
+
+    socket.on("disconnect", () => {
+        console.log("User disconnected:", socket.id);
+    });
+});
+
+server.listen(3000, () => {
+    console.log("Server running on port 3000");
+});
