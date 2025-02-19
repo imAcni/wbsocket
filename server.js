dcc1e75548decdc3ad8a0f9cdd9f962f23e185c9
imAcni <101078007+imAcni@users.noreply.github.com>
2025-02-19 11:53:55 -0500

    asd
    asdasd

--- server.js
+++ server.js
@@ -94,6 +94,14 @@ io.on("connection", (socket) => {
         for (const [lobbyId, players] of Object.entries(activeLobbies)) {
             if (players.includes(socket.id)) {
                 activeLobbies[lobbyId] = players.filter((id) => id !== socket.id);
+                if (activeLobbies[lobbyId].length === 1) {
+                    const remainingPlayer = activeLobbies[lobbyId][0];
+                    
+                    io.to(remainingPlayer).emit("disconnected", {
+                        message: "Other player disconnected"
+                    });
+                }
+
                 if (activeLobbies[lobbyId].length === 0) {
                     console.log(`Lobby ${lobbyId} is empty, deleting the lobby.`);
                     delete activeLobbies[lobbyId];
