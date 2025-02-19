aa9b2ec9e3320def73b65d3a7ae5680e19dfdf7c
imAcni <101078007+imAcni@users.noreply.github.com>
2025-02-18 22:50:12 -0500

    adasd
    adasd

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const { createServer } = require("http");
 const { Server } = require("socket.io");
 const cors = require("cors");
+const { v4: uuidv4 } = require("uuid");
 
 const app = express();
 app.use(cors());
@@ -14,14 +15,35 @@ const io = new Server(server, {
     }
 });
 
+app.get("/create-lobby", (req, res) => {
+    const lobbyId = uuidv4(); // Generate unique lobby ID
+    activeLobbies[lobbyId] = [];
+    res.json({ lobbyId });
+});
+
+const activeLobbies = {};
+
 io.on("connection", (socket) => {
     console.log("A user connected:", socket.id);
 
-    socket.on("playerTyped", (data) => {
-        io.emit("updateGame", data);
-    });
+    socket.on("join-lobby", (lobbyId) => {
+        if (!activeLobbies[lobbyId]) return;
+        activeLobbies[lobbyId].push(socket.id);
 
+        socket.join(lobbyId);
+        console.log(`User ${socket.id} joined lobby ${lobbyId}`);
+
+        if (activeLobbies[lobbyId].length === 2) {
+            io.to(lobbyId).emit("start-game");
+        }
+    });
     socket.on("disconnect", () => {
+        for (const [lobbyId, players] of Object.entries(activeLobbies)) {
+            if (players.includes(socket.id)) {
+                activeLobbies[lobbyId] = players.filter((id) => id !== socket.id);
+                if (activeLobbies[lobbyId].length === 0) delete activeLobbies[lobbyId];
+            }
+        }
         console.log("User disconnected:", socket.id);
     });
 });
